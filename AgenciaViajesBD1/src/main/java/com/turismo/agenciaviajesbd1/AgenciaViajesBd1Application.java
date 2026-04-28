12f47b253ed2f2a455e0f46d6d27652250ca49ad
emmanuelartunduagach-hue <emmanuel_artunduagach@fet.edu.co>
2026-04-28 09:32:19 -0500

    Cargando el primer commit

---
+++ AgenciaViajesBD1/src/main/java/com/turismo/agenciaviajesbd1/AgenciaViajesBd1Application.java
@@ -0,0 +1,13 @@
+package com.turismo.agenciaviajesbd1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenciaViajesBd1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgenciaViajesBd1Application.class, args);
+    }
+
+}
---
+++ AgenciaViajesBD1/src/test/java/com/turismo/agenciaviajesbd1/AgenciaViajesBd1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turismo.agenciaviajesbd1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenciaViajesBd1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
